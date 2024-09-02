61d5c1e2e06a09ac93217a071a87f9f3f2cdb5a7
Fonzy57 <traben@hotmail.fr>
2024-09-02 13:29:45 +0200

    Change langage for app and add icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <link rel="icon" href="./mowgli-tatoo-studio-logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
