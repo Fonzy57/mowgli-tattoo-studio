85fea89ec6cdcf87bf2dbce9175b813bd16fdb0f
Fonzy57 <traben@hotmail.fr>
2024-09-02 13:29:23 +0200

    Add layout in app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// REACT & NEXT
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+// COMPONENTS
+import Layout from "@/components/layout/layout";
+
+// STYLE
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
