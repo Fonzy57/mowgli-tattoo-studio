1bac1fb6cb53cb30a3c5f1242a643d46eed80fe7
Fonzy57 <traben@hotmail.fr>
2024-09-02 16:23:52 +0200

    Add header, header mobile components and a nav items config file

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,39 +2,36 @@
 import Image from "next/image";
 import Link from "next/link";
 
-/* TODO AJOUTER LES LIENS SI JE FAIS DES PAGES OU DES SCROLLS SI JE FAIS DES ANCRES */
-const navLinks = [
-  /* { name: "home", label: "Accueil", url: "#", isActive: false }, */
-  { name: "studio", label: "Le studio", url: "#", isActive: false },
-  { name: "projects", label: "Mes projets", url: "#", isActive: false },
-  { name: "contact", label: "Contact", url: "#", isActive: false },
-];
+// CONFIG
+import { navLinks } from "@/config/nav";
 
 const Header = () => {
   return (
     /* TODO AJOUTER UNE SHADOW QUAND ON COMMENCE A SCROLL */
-    <div className="w-full bg-main min-h-[80px]">
-      <div className="flex items-center justify-between w-[1170px] mx-auto xl:w-[1560px] ">
+    <div className="w-full bg-main min-h-[80px] flex justify-center items-center">
+      <div className="flex items-center justify-between w-[1170px] mx-auto xl:w-[1560px]">
         <Link href={"/"}>
           <Image
-            src={"/mowgli-tatoo-studio-logo.png"}
-            width={80}
-            height={80}
+            src={"/images/mowgli-tatoo-studio-logo.png"}
+            width={70}
+            height={70}
             alt="Logo de Mowgli Studio Tatoo"
           />
         </Link>
-        <ul className="flex gap-8 font-cera uppercase ">
-          {navLinks.map((link, index) => {
-            return (
-              <Link
-                href={link.url}
-                className="font-bold text-xl text-text hover:text-secondary-hover active:text-secondary-active transition duration-300 ease-out"
-              >
-                <li key={index}>{link.label}</li>
-              </Link>
-            );
-          })}
-        </ul>
+        <nav>
+          <ul className="flex gap-8 font-cera uppercase ">
+            {navLinks.map((navitem, index) => {
+              return (
+                <Link
+                  href={navitem.url}
+                  className="font-bold text-xl text-text hover:text-secondary-hover active:text-secondary-active transition duration-300 ease-in-out"
+                >
+                  <li key={index}>{navitem.label}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
     </div>
   );
---
+++ src/config/nav.ts
@@ -0,0 +1,7 @@
+/* TODO AJOUTER LES LIENS SI JE FAIS DES PAGES OU DES SCROLLS SI JE FAIS DES ANCRES */
+export const navLinks = [
+  /* { name: "home", label: "Accueil", url: "#", isActive: false }, */
+  { name: "studio", label: "Le studio", url: "#", isActive: false },
+  { name: "projects", label: "Mes projets", url: "#", isActive: false },
+  { name: "contact", label: "Contact", url: "#", isActive: false },
+];
