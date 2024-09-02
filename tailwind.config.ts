0dea927fe7b6fecb5f2d63293b9da4b1d27032d8
Fonzy57 <traben@hotmail.fr>
2024-09-02 13:19:03 +0200

    Add tailwind config colors and responsive breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "768px", //  tablet portrait => @media (min-width: 720px)
+      md: "1024px", // tablet paysage => @media (min-width: 1020px)
+      lg: "1440px", // laptop => @media (min-width: 1435px)
+      xl: "1920px", // desktop => @media (min-width: 1435px)
+    },
+    colors: {
+      white: "#FFF",
+      main: {
+        DEFAULT: "#FFD161",
+        hover: "#FFBA45",
+        active: "#E6C053",
+        altLight: "#FFF4E1",
+        altDark: "#D9B44A",
+      },
+      secondary: {
+        DEFAULT: "#FA9023",
+        hover: "#E97818",
+        active: "#D16F15",
+        altLight: "#FA9023",
+        altDark: "#FA9023",
+      },
+      border: {
+        light: "#F1CEA1",
+        dark: "#B85C0E",
+      },
+      text: "#46320F",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
