b57d9ce3291c4fb9d0fc3fe7a3c20dfe4cfe365d
Fonzy57 <traben@hotmail.fr>
2024-09-03 13:11:46 +0200

    Add links in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,39 +1,83 @@
-import { version } from "@/config/version";
+// REACT & NEXT
 import Image from "next/image";
+import Link from "next/link";
 
-const year = 2024;
+// COMPONENTS
+import { CustomIcon } from "../icons/icon";
+import { IconName } from "../icons/icon.enum";
 
-/* TODO VOIR SI JE METS DES LIENS DE NAVIGATION DANS LE FOOTER OU PAS */
+// CONFIG
+import { version } from "@/config/version";
 
-/* TODO AJOUTER LIENS VERS L'INSTA DE FRED POUR LES PHOTOS ET VERS MON LINKEDIN OU SITE POUR MOI */
+const year = new Date().getFullYear();
 
-/* TODO AJOUTER LES LIENS VERS LES RESEAUX ET CHERCHER LES ICONES SUR REACT ICONS */
-
-/* TODO FAIRE UN COMPOSANT ICONS COMME POUR LE DASH */
-
-/* TODO INSTA DE Hildevert Frédéric : 
-  https://www.facebook.com/people/Newbypics/61563660336691/
-  https://www.instagram.com/newby.pics/
-*/
+const about = [
+  {
+    name: "mail",
+    label: "mail.mowgli@gmail.com", // TODO METTRE LA BONNE ADRESSE
+    icon: <CustomIcon name={IconName.Envelope} size={20} />,
+  },
+  {
+    name: "address",
+    label: "2a Rue des Pépinières,",
+    label2: "57050 Longeville-lès-Metz",
+    icon: <CustomIcon name={IconName.Location} size={20} />,
+  },
+];
 
 const socialsMowgli = [
   {
     name: "insta",
     label: "Instagram",
-    icon: "ICI METTRE LES ICONES",
+    icon: <CustomIcon name={IconName.Instagram} size={20} />,
     url: "https://www.instagram.com/mowgli_tattoostudio/",
   },
   {
     name: "facebook",
     label: "Facebook",
-    icon: "ICI METTRE LES ICONES",
+    icon: <CustomIcon name={IconName.Facebook} size={20} />,
     url: "https://www.facebook.com/mowglitattoostudio",
+  },
+];
+
+const other = [
+  {
+    name: "grigri",
+    label: "Le Grigri Tatoo",
+    socials: [
+      {
+        name: "instagram",
+        label: <CustomIcon name={IconName.Instagram} size={20} />,
+        url: "https://www.instagram.com/legrigritattoo/",
+      },
+      {
+        name: "wp",
+        label: <CustomIcon name={IconName.Wordpress} size={20} />,
+        url: "https://legrigritattoofr.wordpress.com/",
+      },
+    ],
+  },
+  {
+    name: "newby",
+    label: "Newby.pics",
+    socials: [
+      {
+        name: "instagram",
+        label: <CustomIcon name={IconName.Instagram} size={20} />,
+        url: "https://www.instagram.com/newby.pics/",
+      },
+      {
+        name: "fb",
+        label: <CustomIcon name={IconName.Facebook} size={20} />,
+        url: "https://www.facebook.com/people/Newbypics/61563660336691/",
+      },
+    ],
   },
 ];
 
 const Footer = () => {
   const titleStyle =
-    "font-cera uppercase font-bold text-lg text-text cursor-default";
+    "font-cera uppercase font-bold text-xl text-text cursor-default";
 
   return (
     <div className="bg-main pt-20 pb-6">
@@ -44,32 +88,85 @@ const Footer = () => {
           width={286}
           height={107}
         />
-        <div className="flex justify-between w-[600px]">
+        <div className="flex justify-between w-1/2">
           <div className="">
             <h4 className={titleStyle}>A propos</h4>
-            <div className="mt-2 text-text">
-              <div>icone + texte</div>
-              <div>icone + texte</div>
+            <div className="mt-2 text-text flex flex-col gap-y-3">
+              {about.map((item, index) => {
+                return (
+                  <div key={index} className="flex items-center gap-x-3">
+                    <div>{item.icon}</div>
+                    <div>
+                      <p>{item.label}</p>
+                      {item.label2 && <p>{item.label2}</p>}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="">
             <h4 className={titleStyle}>Reseaux</h4>
-            <div className="mt-2 text-text">
-              <div>icone insta </div>
-              <div>icone fb</div>
+            <div className="mt-2 text-text flex flex-col gap-y-3">
+              {socialsMowgli.map((item, index) => {
+                return (
+                  <Link
+                    href={item.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-secondary-hover active:text-secondary-active transition duration-300 ease-in-out"
+                  >
+                    <div key={index} className="flex items-center gap-x-3">
+                      <div>{item.icon}</div>
+                      <p>{item.label}</p>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
           </div>
           <div className="">
-            <h4 className={titleStyle}>Partenaires</h4>
-            <div className="mt-2 text-text">
-              <div>Grigri tatoo + lien</div>
-              <div>Photographe + lien</div>
+            <h4 className={titleStyle}>Autres</h4>
+            <div className="mt-2 text-text flex flex-col gap-y-3">
+              {other.map((item, index) => {
+                return (
+                  <div key={index} className="flex items-center gap-x-4">
+                    <p className="cursor-default">{item.label}</p>
+                    <div className="flex items-center gap-x-2">
+                      {item.socials.map((social, index2) => {
+                        return (
+                          <Link
+                            href={social.url}
+                            key={index2}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-secondary-hover active:text-secondary-active transition duration-300 ease-in-out"
+                          >
+                            <div>{social.label}</div>
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
       <p className="text-text text-xs text-center mt-20">
         © Mowgli Tatoo Studio {year} - Tous droits réservés | v{version}
+      </p>
+      <p className="text-text text-xs text-center mt-2">
+        Site développé par{" "}
+        <Link
+          href="https://www.linkedin.com/in/stephane-scheeres/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:no-underline hover:text-secondary-hover active:text-secondary-active transition duration-300 ease-in-out"
+        >
+          Stéphane Scheeres
+        </Link>
       </p>
     </div>
   );
