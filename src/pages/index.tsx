89da0ac3be580df1b1109e36ec9f765ff481b403
Fonzy57 <traben@hotmail.fr>
2024-09-04 09:19:20 +0200

    Add hero and about section for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,32 @@ import Link from "next/link";
 
 // COMPONENTS
 
-// CONFIG
-import { version } from "@/config/version";
+// STYLES
+import styles from "../styles/hero.module.css";
 
 const Home = () => {
   return (
     <>
-      <section className="flex py-52 justify-center gap-x-32 items-center">
+      <section
+        className={`${styles.hero} flex justify-center items-center h-[calc(100vh-80px)]`}
+      >
+        <h1 className="font-cera text-center text-9xl font-semibold text-main">
+          The Mowgli Tattoo Studio
+        </h1>
+      </section>
+      <section className="flex justify-center items-center gap-x-32 py-48">
         <div className="w-[750px]">
-          <h1 className="font-cera text-center text-6xl font-semibold text-main">
-            The Mowgli Tattoo Studio
-          </h1>
-          <div className="flex flex-col gap-y-4 mt-8">
-            <p className="text-text">
+          <h2 className="font-cera text-center text-7xl font-semibold text-main">
+            A propos de moi
+          </h2>
+          <div className="flex flex-col gap-y-4 mt-20">
+            <p className="text-text-light">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam,
               optio. Aperiam, non quae provident modi qui corporis! Est,
               accusantium commodi numquam culpa, dicta exercitationem
               necessitatibus cumque, velit atque eum quibusdam?
             </p>
-            <p className="text-text">
+            <p className="text-text-light">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam,
               optio. Aperiam, non quae provident modi qui corporis! Est,
               accusantium commodi numquam culpa, dicta exercitationem
@@ -30,13 +37,16 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <Image
-          src="/pictures/pierre-emmanuel-bauer-hero.jpg"
-          alt="Photo en noir et blanc de Pierre-Emmanuel Bauer, aka Mowgli"
-          width={700}
-          height={561}
-          className="rounded-lg shadow-xl"
-        />
+        <div className="relative">
+          <Image
+            src="/pictures/pierre-emmanuel-bauer-hero.jpg"
+            alt="Photo en noir et blanc de Pierre-Emmanuel Bauer, aka Mowgli"
+            width={700}
+            height={561}
+            className="rounded-lg"
+          />
+          <div className="absolute inset-0 rounded-lg shadow-full-main animate-pulse"></div>
+        </div>
       </section>
     </>
   );
