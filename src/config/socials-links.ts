2e7a42aec0962024f21733ac119214c97e05e84d
Fonzy57 <traben@hotmail.fr>
2024-09-05 09:46:55 +0200

    Add socials links in config

--- src/config/socials-links.ts
+++ src/config/socials-links.ts
@@ -1,4 +1,9 @@
-export const socialsLinks = {
+export const socialsLinksMowgli = {
   instagram: "https://www.instagram.com/mowgli_tattoostudio/",
   facebook: "https://www.facebook.com/mowglitattoostudio",
+};
+
+export const socialsLinksNewbyPics = {
+  instagram: "https://www.instagram.com/newby.pics/",
+  facebook: "https://www.facebook.com/people/Newbypics/61563660336691/",
 };
