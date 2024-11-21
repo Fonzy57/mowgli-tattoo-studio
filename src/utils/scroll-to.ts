5a7c9a20e803dcbc49804249c7f689333dc5b3de
Fonzy57 <traben@hotmail.fr>
2024-11-21 11:15:26 +0100

    Fix scroll to function for medium screens

--- src/utils/scroll-to.ts
+++ src/utils/scroll-to.ts
@@ -1,8 +1,14 @@
 export const handleClickAnchor = (id: string, offset: number) => {
   const element_to_scroll_to = document.getElementById(id);
   if (element_to_scroll_to) {
+    // CHECKING WINDOW WIDTH
+    const screenWidth = window.innerWidth;
+
+    // SET OFFSET TO 0 IF SCREEN UNDER 1440 PX
+    const adjustedOffset = screenWidth < 1440 ? 0 : offset;
+
     window.scrollTo({
-      top: element_to_scroll_to.offsetTop - offset,
+      top: element_to_scroll_to.offsetTop - adjustedOffset,
       behavior: "smooth",
     });
   }
