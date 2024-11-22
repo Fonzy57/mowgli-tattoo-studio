b7568848130c788436d5a890b84ba9b3b8a1a613
Fonzy57 <traben@hotmail.fr>
2024-11-22 09:36:05 +0100

    Add canonical link for seo component

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 // CONFIG
 import { seoConfig } from "@/config/seo";
+import { useRouter } from "next/router";
 
 // TYPING
 type SeoPage = keyof typeof seoConfig;
@@ -12,13 +13,20 @@ interface SeoProps {
 }
 
 const Seo = ({ page }: SeoProps) => {
+  const router = useRouter();
+
   const seoTitle = seoConfig[page].title;
   const seoDescription = seoConfig[page].description;
+  const currentUrl =
+    typeof window !== "undefined"
+      ? `${window.location.origin}${router.asPath}`
+      : "";
 
   return (
     <Head>
       <title>{seoTitle}</title>
       <meta name="description" content={seoDescription} />
+      <link rel="canonical" href={currentUrl} />
     </Head>
   );
 };
