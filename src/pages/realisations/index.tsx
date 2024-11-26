6ae6df55c602148a2968d425f9ceaf879e175c09
Fonzy57 <traben@hotmail.fr>
2024-11-26 09:49:06 +0100

    Add instagram API

--- src/pages/realisations/index.tsx
+++ src/pages/realisations/index.tsx
@@ -1,9 +1,14 @@
 // NEXT
 import Link from "next/link";
+import { GetServerSideProps } from "next";
+
+// CONFIG
+import { socialsLinksMowgli } from "@/config/socials-links";
 
 // COMPONENTS
 import PostLoader from "@/components/skeleton-loader/gallery-skeleton";
 import Seo from "@/components/seo/seo";
+import { LinkButton } from "@/components/button/link-button";
 
 // TYPING
 export interface PostProps {
@@ -28,7 +33,7 @@ const RealisationsPage = ({ data, error }: PostsProps) => {
     <>
       <Seo page="realisations" />
       <h1 className="font-cera text-center font-semibold text-main pt-16 text-4xl sm:pt-20 sm:text-5xl md:text-6xl lg:text-8xl">
-        Mes tatouages
+        Mes derniers tatouages
       </h1>
       <p className="px-5 text-text-light text-justify w-full mx-auto mt-8 sm:text-lg sm:mt-10 md:px-0 md:text-left md:w-[700px] lg:mt-12">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
@@ -60,14 +65,29 @@ const RealisationsPage = ({ data, error }: PostsProps) => {
       ) : (
         <PostLoader numberOfPosts={12} />
       )}
+      <div className="w-max mx-auto mb-20">
+        <LinkButton url={socialsLinksMowgli.instagram} blank={true}>
+          Voir tous mes tatouages
+        </LinkButton>
+      </div>
     </>
   );
 };
 
 export default RealisationsPage;
 
-export const getServerSideProps = async () => {
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`;
+export const getServerSideProps: GetServerSideProps<PostsProps> = async () => {
+  // NUMBER OF POSTS LIMIT
+  const limit = 40;
+
+  /* TODO FAIRE EN SORTE DE FILTRER TOUT CE QUI N'EST PAS UNE VIDEO
+    ET FAIRE EN SORTE D'EN AVOIR TOUJOURS 40
+    TODO UTILISER RTK QUERY POUR FAIRE CA AVEC UN CACHE DE 24h minimum ou invalider le cache si les données changes
+    VOIR SI JE FAIS AUSSI UN FILTRE SUR LES CAPTIONS QUI NE PARLE QUE DES TATTOOS
+  */
+
+  // Construire l'URL en fonction du curseur disponible
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&limit=${limit}&access_token=${process.env.INSTAGRAM_TOKEN}`;
 
   let data = [];
   let errorAPI = false;
