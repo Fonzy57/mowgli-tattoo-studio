8b9f59d91caee1e27ca66c1f61edfb79f8609524
Fonzy57 <traben@hotmail.fr>
2024-11-27 10:11:24 +0100

    Add an error display for home page

---
+++ src/components/skeleton-loader/error-gallery-home.tsx
@@ -0,0 +1,26 @@
+// COMPONENTS
+import { LinkButton } from "../button/link-button";
+
+// CONFIG
+import { socialsLinksMowgli } from "@/config/socials-links";
+
+const ApiErrorGalleryHome = () => {
+  return (
+    <div className="flex justify-center items-center max-w-[950px] px-2 py-10 mx-auto mt-20 bg-[#7f7f7f]/50 rounded-lg md:px-0">
+      <div className="flex flex-col items-center gap-y-10">
+        <div className="text-text-light text-center flex flex-col items-center gap-y-4 sm:gap-y-2 lg:text-lg">
+          <p>Il y a actuellement un problème pour charger les données 😥</p>
+          <p>
+            Pas d’inquiétude, vous pouvez toujours voir mes tatouages sur
+            Instagram 👇
+          </p>
+        </div>
+        <LinkButton url={socialsLinksMowgli.instagram} blank={true}>
+          Aller sur Instagram
+        </LinkButton>
+      </div>
+    </div>
+  );
+};
+
+export default ApiErrorGalleryHome;
