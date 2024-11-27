0ba8beef7623f53cfeb39ba2ee737cc9539ed42b
Fonzy57 <traben@hotmail.fr>
2024-11-27 10:25:45 +0100

    Change error text

--- src/components/skeleton-loader/error-gallery-display.tsx
+++ src/components/skeleton-loader/error-gallery-display.tsx
@@ -9,7 +9,9 @@ const ApiErrorGalleryDisplay = () => {
     <div className="flex justify-center items-center max-w-[950px] px-2 py-10 mx-auto mt-20 bg-[#7f7f7f]/50 rounded-lg md:px-0">
       <div className="flex flex-col items-center gap-y-10">
         <div className="text-text-light text-center flex flex-col items-center gap-y-4 sm:gap-y-2 lg:text-lg">
-          <p>Il y a actuellement un problème pour charger les données 😥</p>
+          <p>
+            Oups ! Une erreur est survenue lors du chargement des données 😥
+          </p>
           <p>
             Pas d’inquiétude, vous pouvez toujours voir mes tatouages sur
             Instagram 👇
