775284451835497f1af681bfa7406879ade51363
Fonzy57 <traben@hotmail.fr>
2024-11-29 15:37:10 +0100

    Add social links for grigri in config and add virgule for mowgli

--- src/config/socials-links.ts
+++ src/config/socials-links.ts
@@ -1,9 +1,15 @@
 export const socialsLinksMowgli = {
   instagram: "https://www.instagram.com/mowgli_tattoostudio/",
   facebook: "https://www.facebook.com/mowglitattoostudio",
+  virgule: "https://www.instagram.com/la_virgule__/",
 };
 
 export const socialsLinksNewbyPics = {
   instagram: "https://www.instagram.com/newby.pics/",
   facebook: "https://www.facebook.com/people/Newbypics/61563660336691/",
+};
+
+export const socialsLinksGrigri = {
+  instagram: "https://www.instagram.com/legrigritattoo/",
+  site: "https://legrigritattoofr.wordpress.com/",
 };
