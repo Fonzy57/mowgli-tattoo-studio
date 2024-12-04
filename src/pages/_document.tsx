d53d19a06f7e698618dc34495ef2047c38f076a8
Fonzy57 <traben@hotmail.fr>
2024-12-04 17:36:31 +0100

    Add meta theme-color for mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,7 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <link rel="icon" href="./images/mowgli-tattoo-studio-logo.png" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        ></meta>
-        {/* TODO TEST MOBILE SUPPRIMER SI PAS BON */}
-        {/* <meta name="theme-color" content="#1E1E1E" /> */}
+        <meta name="theme-color" content="#1E1E1E" />
       </Head>
       <body>
         <Main />
