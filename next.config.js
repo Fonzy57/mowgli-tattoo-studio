6a183b1071842645ace5aa13ed2da28426170b68
Fonzy57 <traben@hotmail.fr>
2024-12-04 12:04:08 +0100

    Fix base path for all images for github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV == "production";
+
+const nextConfig = {
+  /* ------------------------------------------------- */
+  /* TODO REMOVE WHEN DEPLOY ON OTHER THAN GITHUB PAGE */
+  basePath: isProd ? '/mowgli-tattoo-studio' : '',
+  assetPrefix: isProd ? '/mowgli-tattoo-studio/' : '',
+  /* ------------------------------------------------- */
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,18 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    /* ------------------------------------------------- */
+    /* TODO REMOVE WHEN DEPLOY ON OTHER THAN GITHUB PAGE */
+    'postcss-url': {
+      url: ({ url }) => {
+        const isProd = process.env.NODE_ENV === 'production';
+        const basePath = isProd ? '/mowgli-tattoo-studio' : '';
+        if (url.startsWith('/')) {
+          return `${basePath}${url}`;
+        }
+        return url;
+      },
+    },
+    /* ------------------------------------------------- */
   },
-}
+};
