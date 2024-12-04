efe9d0374f73433f60649fcce4a5c9c3a02a9aff
Fonzy57 <traben@hotmail.fr>
2024-12-04 16:23:19 +0100

    Add comment

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <link rel="icon" href="./images/mowgli-tattoo-studio-logo.png" />
+        {/* TODO SUPPRIMER SI PADDING FONCTIONNE BIEN */}
+        {/* <meta name="theme-color" content="#FFD161" /> */}
       </Head>
       <body>
         <Main />
