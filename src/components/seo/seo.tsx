6f1ced4bbe314291b70abe96eefe8db5705f31d3
Fonzy57 <traben@hotmail.fr>
2024-12-05 14:51:51 +0100

    Remove useless code in seo

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 
 // CONFIG
 import { seoConfig, seoStructuredData } from "@/config/seo";
-import { useRouter } from "next/router";
 
 // TYPING
 type SeoPage = keyof typeof seoConfig;
@@ -13,14 +12,8 @@ interface SeoProps {
 }
 
 const Seo = ({ page }: SeoProps) => {
-  const router = useRouter();
-
   const seoTitle = seoConfig[page].title;
   const seoDescription = seoConfig[page].description;
-  const currentUrl =
-    typeof window !== "undefined"
-      ? `${window.location.origin}${router.asPath}`
-      : "";
 
   return (
     <Head>
