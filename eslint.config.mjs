53df82f2a3ba81bca6598e69a545e86528857297
Fonzy57 <traben@hotmail.fr>
2025-11-06 11:48:07 +0100

    Add linter to project and fix config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  nextPlugin.configs.recommended,
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
+      "node_modules",
+      ".next",
+      "out",
+      "build",
+      "dist",
+      "coverage",
       "next-env.d.ts",
     ],
   },
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ];
-
-export default eslintConfig;
