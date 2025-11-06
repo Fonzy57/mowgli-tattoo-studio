c5eb54ccb67a4f5e1da6f502b450ff7d5d04fe2a
Fonzy57 <traben@hotmail.fr>
2025-11-06 08:29:05 +0100

    Remove github pages variable in next and postcss config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-// USE TO CHECK IF IS DEPLOYING ON GITHUB PAGES
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig = {
-  basePath: isGithubPages ? '/mowgli-tattoo-studio' : '',
-  assetPrefix: isGithubPages ? '/mowgli-tattoo-studio/' : '',
   reactStrictMode: true,
   output: 'standalone',
-  images: {
-    unoptimized: true, // TODO: remove when hosted on real server
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isGithubPages ? '/mowgli-tattoo-studio' : '',
-  },
+  compress: true,
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
-// USE TO CHECK IF IS DEPLOYING ON GITHUB PAGES
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(isGithubPages && {
-      'postcss-url': {
-        url: ({ url }) => {
-          const basePath = '/mowgli-tattoo-studio';
-          if (url.startsWith('/')) {
-            return `${basePath}${url}`;
-          }
-          return url;
-        },
-      },
-    }),
   },
 };
